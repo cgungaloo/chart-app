64bdae73aa47c49ae083042236faae5a5296e8cc
cgungaloo <cgungaloo@yahoo.com>
2020-05-19 20:30:02 +0100

    react-chart and random

---
+++ src/ChartRender.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react'
+import { Chart } from 'react-charts'
+
+function ChartRender(){
+
+	const min = 1;
+    const max = 100;
+
+    function randomNum(){
+    	return min + Math.random() * (max - min);
+    }
+
+	const datarand =(
+		() => [[0, randomNum()], [1, randomNum()], [2, randomNum()], [3, randomNum()], [4, randomNum()]]
+	)
+
+	const data = React.useMemo(
+    () => [
+      {
+        label: 'Series 1',
+        data: datarand()
+      },
+      {
+        label: 'Series 2',
+        data: datarand()
+      }
+    ],
+    []
+  )
+
+	const axes = React.useMemo(
+    () => [
+      { primary: true, type: 'time', position: 'bottom' },
+      { type: 'linear', position: 'left'}
+    ],
+    []
+  )
+
+	return(
+		<div
+      style={{
+        width: '400px',
+        height: '300px'
+      }}
+    >
+      <Chart data={data} axes={axes} />
+    </div>
+		);
+}
+
+export default ChartRender
