e38cd8b087612497a2697e8316abfed7c5736ed7
cgungaloo <cgungaloo@yahoo.com>
2020-06-21 18:14:15 +0100

    Added number of years

--- src/Data/Charter.js
+++ src/Data/Charter.js
@@ -5,10 +5,10 @@ export default class LineDemo extends Component {
 
   state ={
     data:{
-      labels: ["Year 1", "Year 2", "Year 3", "Year 4", "Year 5", "Year 6","Year 7","Year 8","Year 9","Year 10"],
+      labels: [],
       datasets: [
         {
-          label: "First dataset",
+          label: "Total Amount",
           data: [],
           fill: false,
           backgroundColor: "rgba(75,192,192,0.2)",
@@ -21,46 +21,62 @@ export default class LineDemo extends Component {
   componentDidMount(){
       var initial_value =this.props.initial_amount
       let data = this.state.data
+      var number_of_years = this.props.number_of_years
+      data.labels = this.graphYearAxis(number_of_years)
       data.datasets[0].data = this.compoundList(initial_value)
       console.log(data.datasets[0].data[0])
       this.setState({ ...data});
   }
 
   componentDidUpdate(prevProps){
-    if (prevProps.interest_rate !== this.props.interest_rate || prevProps.initial_amount !== this.props.initial_amount) {
+    if (prevProps !== this.props)  {
       var initial_value =this.props.initial_amount
+      var number_of_years = this.props.number_of_years
+
       let data = this.state.data;
+      data.labels = this.graphYearAxis(number_of_years)
       data.datasets[0].data = this.compoundList(initial_value)
       console.log(data.datasets[0].data[0])
       this.setState({ ...data });
     }
   }
 
+  graphYearAxis(number_of_years){
+    var xAxis =[]
+    for(var i=1; i <= number_of_years; i++){
+      xAxis.push("Year " + i)
+    }
+    return xAxis;
+  }
 
   compoundList(initial_value){
     const annual_payments = parseFloat(this.props.annual_payments)
-    var current_value = parseFloat(initial_value) + annual_payments
+    var current_value = parseFloat(initial_value)
     const interest_rate = parseFloat(this.props.interest_rate)
+    const number_of_years = this.props.number_of_years;
+
     var compoundList =[];
 
-    for (var i =0; i < 10; i++){
-      current_value = this.calculateCompound(current_value, interest_rate)
+    for (var i =0; i < number_of_years; i++){
+      current_value = this.calculateCompound(current_value, interest_rate,annual_payments)
       compoundList.push(current_value);
     }
-    console.log(compoundList)
     return compoundList;
   }
 
-  calculateCompound(current_value, interest_rate){
+  calculateCompound(current_value, interest_rate, annual_payments){
 
-    return current_value + ((current_value/100) * interest_rate)
+    return current_value + ((current_value/100) * interest_rate) + annual_payments
   }
 
 render(){
   return (
       <div>
         <Line data={this.state.data} ref={(reference) => this.reference = reference} width={5} height={1}/>
-        <h1>Interest Rate: {this.props.interest_rate}</h1>
+        <h1>% Interest Rate: {this.props.interest_rate}</h1>
+        <h1>Initial Value: {this.props.initial_amount}</h1>
+        <h1>annual_payments: {this.props.annual_payments}</h1>
+        <h1>number_of_years: {this.props.number_of_years}</h1>
       </div>
     );
   }
--- src/Data/DataVars.js
+++ src/Data/DataVars.js
@@ -7,7 +7,8 @@ class DataVars extends Component{
 		this.state = {
 			interest_rate:0.05,
 			initial_amount:1000,
-			annual_payments:100
+			annual_payments:100,
+			number_of_years:10
 		}
 		this.handleSubmit = this.handleSubmit.bind(this)
 	}
@@ -19,7 +20,8 @@ class DataVars extends Component{
 		this.setState({
 			 interest_rate: inputs.interest_rate.value,
 			 initial_amount: inputs.initial_amount.value,
-			 annual_payments: inputs.monthly_payments.value
+			 annual_payments: inputs.annual_payments.value,
+			 number_of_years: inputs.number_of_years.value
 		 },
 	 	function() { console.log("setState completed", this.state) });
 
@@ -27,19 +29,22 @@ class DataVars extends Component{
 
 	render(){
 
-		const {interest_rate,initial_amount} = this.state
+		const {interest_rate,initial_amount,annual_payments,number_of_years} = this.state
 		return(
 		<div>
 
-			<Charter interest_rate={interest_rate} initial_amount={initial_amount}/>
+			<Charter interest_rate={interest_rate} initial_amount={initial_amount} annual_payments={annual_payments}
+			number_of_years={number_of_years}/>
 
 			<form onSubmit={this.handleSubmit}>
-				<label for="interest_rate">Interest Rate</label>
+				<label for="interest_rate">% Interest Rate</label>
 				<input type="text" name="interest_rate" id="interest_rate"/><br/>
 				<label for="initial_amount">Initial Value</label>
 				<input type="text" name="initial_amount" id="initial_amount"/><br/>
 				<label for="annual_payments">Annual Payments</label>
 				<input type="text" name="annual_payments" id="annual_payments"/><br/>
+				<label for="number_of_years">Number of years</label>
+				<input type="number" name="number_of_years" id="number_of_years" min="1" max="100"/><br/>
 				<input type="submit" value="Submit" />
 			</form>
 		</div>
